7b9016f6f463ad798daae3f66f09e3f3c3c45f5b
Yuki Ito <sn0wsxx@gmail.com>
2017-08-13 00:07:31 +0900

    retouch production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,5 +77,5 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
   #remember to change localhost to actual
-  config.action_mailer.default_url_options = { host: 'localhost', port: 3000
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
 end
